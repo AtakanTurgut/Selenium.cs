a81cdbb393ca1b2ebbe58722f0f3fb9a73b96592
AtakanTurgut <atakan.trgt@hotmail.com>
2025-07-22 14:07:49 +0300

    Selenium simple application

---
+++ SeleniumFirstApp/SeleniumFirstApp/SimpleApplicationRunner.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumFirstApp
+{
+    public class SimpleApplicationRunner
+    {
+        public static void Main(string[] args)
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();  // Windows Full Screen
+
+            driver.Navigate().GoToUrl("https://github.com/");
+
+            IWebElement searchBox = driver.FindElement(By.CssSelector(".search-input"));    // class
+            searchBox.Click();
+
+            IWebElement searchInput = driver.FindElement(By.CssSelector("#query-builder-test"));    // id
+            searchInput.SendKeys("selenium");
+            searchInput.SendKeys(Keys.Enter);
+
+            //driver.Quit();
+        }
+    }
+}
